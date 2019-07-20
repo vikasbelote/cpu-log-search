9595ba051bc262ae1c32b3994798eaf0e5078041
vikasbelote <vikasb0207@gmail.com>
2019-07-21 03:39:34 +0530

    complete code

--- src/main/java/com/cpu/cpulogsearch/CpuLogSearchApplication.java
+++ src/main/java/com/cpu/cpulogsearch/CpuLogSearchApplication.java
@@ -1,17 +1,50 @@
 package com.cpu.cpulogsearch;
 
+import java.util.List;
+import java.util.Scanner;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.scheduling.annotation.EnableScheduling;
+
+import com.cpu.cpulogsearch.service.CpuLogService;
+import com.cpu.log.utils.dto.CpuLogDTO;
 
 @SpringBootApplication
-@EnableScheduling
-@EnableJpaRepositories
 public class CpuLogSearchApplication {
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(CpuLogSearchApplication.class, args);
+		
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.print("Cpu count equals and greater than : ");
+		String cpuCount = scanner.nextLine();
+		
+
+		System.out.print("Cpu usage equeals and greater than : ");
+		String cpuUsage = scanner.nextLine();
+		
+		
+		CpuLogService cpuLogService = new CpuLogService();
+		List<CpuLogDTO> cpuLogDTOList = cpuLogService.getCpuLogDTOList(cpuCount, cpuUsage);
+		
+		System.out.println("-----------------------------");
+		for(CpuLogDTO cpuLogDTO : cpuLogDTOList) {
+			System.out.println("CPU Count          : " + cpuLogDTO.getCpuCount());
+			System.out.println("CPU Usage          : " + cpuLogDTO.getCpuUsage());
+			System.out.println("IP Address         : " + cpuLogDTO.getIpAddress());
+			System.out.println("CPU Utilized Time  : " + cpuLogDTO.getCpuUtilizedTime());
+			System.out.println("CPU Available Time : " + cpuLogDTO.getCpuAvailableTime());
+			System.out.println("CPU Current Time   : " + cpuLogDTO.getCurrentDateTime());
+			System.out.println("--------------------------");
+		}
+		
+		
+		
+
+		scanner.close();
 	}
 
 }
--- src/main/java/com/cpu/cpulogsearch/service/CpuLogService.java
+++ src/main/java/com/cpu/cpulogsearch/service/CpuLogService.java
@@ -1,37 +1,41 @@
 package com.cpu.cpulogsearch.service;
 
-import java.util.Date;
-import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.cpu.cpulogsearch.entity.CpuLogEntity;
-import com.cpu.cpulogsearch.repository.CpuLogRepository;
+import com.cpu.log.utils.CaptureCpuLog;
 import com.cpu.log.utils.dto.CpuLogDTO;
 
 @Service
 public class CpuLogService {
 	
+	private static List<CpuLogDTO> cpuLogDTOList = new ArrayList<CpuLogDTO>();
 	
-	@Autowired
-	private CpuLogRepository cpuLogRepository;
-	
-	private static final SimpleDateFormat dateFormatForTime = new SimpleDateFormat("HH:mm:ss");
-	
-	private static final SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-	
-	
-	public void saveCpuLog(CpuLogDTO cpuLogDTO  ) {
-		
-		CpuLogEntity cpuLog = new CpuLogEntity();
-		cpuLog.setCpuAvailableTime( cpuLogDTO.getCpuAvailableTime() );
-		cpuLog.setCpuCount( cpuLogDTO.getCpuCount() );
-		cpuLog.setCpuUsage(cpuLogDTO.getCpuUsage());
-		cpuLog.setCpuUtilizedTime(cpuLogDTO.getCpuUtilizedTime());
-		cpuLog.setCurrentDate(new Date());
-		
-		cpuLog = cpuLogRepository.save(cpuLog);
+	static {
+		CaptureCpuLog captureCpuLog = new CaptureCpuLog();
+		cpuLogDTOList = captureCpuLog.getCpuLogDTOList();
 	}
-
+	
+	public List<CpuLogDTO> getCpuLogDTOList(String cpuCount, String cpuUsage) {
+		
+		Double cpuCountDoubleValue = Double.parseDouble(cpuCount);
+		Double cpuUsages = Double.parseDouble(cpuUsage);
+		
+		List<CpuLogDTO> searchList = new ArrayList<CpuLogDTO>();
+		
+		for(CpuLogDTO cpuLogDTO : cpuLogDTOList) {
+			
+			if(cpuLogDTO.getCpuCount() >= cpuCountDoubleValue) {
+				searchList.add(cpuLogDTO);
+				continue;
+			}
+			
+			if(cpuLogDTO.getCpuUsage() >= cpuUsages) {
+				searchList.add(cpuLogDTO);
+			}
+		}	
+		return cpuLogDTOList;
+	}
 }
